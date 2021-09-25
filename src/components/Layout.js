6ffd4587fbeb9c072affa2d5a43809326ef7701c
syawaljasira <syawaljasira12@Gmail.com>
2021-09-25 14:14:54 +0700

    Finished Gatsby Tutorial Part 4

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,22 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-} from './layout.module.css';
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
       <title>{pageTitle}</title>
+      <header className={siteTitle}> {data.site.siteMetadata.title} </header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -22,6 +27,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link className={navLinkText} to="/about">
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // Step 1: Import React
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/Layout';
 
@@ -7,6 +8,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Photo by Spacejoy on Unsplash"
+        src="../images/design-interior.jpg"
+      />
     </Layout>
   );
 };
